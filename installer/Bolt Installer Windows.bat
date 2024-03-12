cadeebda957fc6604417f08f62ba0c00feb1870d
GrisKillPR <killian.lobgeois@gmail.com>
2024-03-12 15:52:54 +0100

    remove immersive portal part

--- installer/Bolt Installer Windows.bat
+++
@@ -1,7 +0,0 @@
-mkdir %appdata%\.Bolt
-
-cd %appdata%\.Bolt
-
-powershell -ExecutionPolicy Unrestricted -command "wget "https://raw.githubusercontent.com/GrisKillPR/Bolt1.20.1/Bolt_ImmersivePortal/installer/Bolt_installeur_1.20.1.ps1" -Outfile jesus.ps1" 
-
-PowerShell -ExecutionPolicy Unrestricted -command ".\jesus.ps1"
